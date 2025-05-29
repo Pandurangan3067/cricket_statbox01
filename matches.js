1af36c0f99b60911a9622167522164e3ce91f310
Pandurangan3067 <td.pandurangan07@gmail.com>
2025-05-29 14:24:19 +0530

    matches.js

---
+++ matches.js
@@ -0,0 +1,33 @@
+const matches = [
+    {
+      id: 1,
+      date: "2025-05-25",
+      team1: "India",
+      team2: "Australia",
+      score1: "289/8",
+      score2: "270/10",
+      winner: "India",
+      manOfTheMatch: "Virat Kohli"
+    },
+    {
+      id: 2,
+      date: "2025-05-25",
+      team1: "Pakistan",
+      team2: "England",
+      score1: "310/6",
+      score2: "312/4",
+      winner: "England",
+      manOfTheMatch: "Joe Root"
+    },
+    {
+      id: 3,
+      date: "2025-05-24",
+      team1: "South Africa",
+      team2: "New Zealand",
+      score1: "265/9",
+      score2: "267/6",
+      winner: "New Zealand",
+      manOfTheMatch: "Kane Williamson"
+    }
+  ];
+  
